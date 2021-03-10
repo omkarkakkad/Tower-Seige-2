def063d76c1de632e70e30fb2541ed994882ed4e
Omkar Kakkad <72384721+omkarkakkad@users.noreply.github.com>
2021-03-10 12:55:56 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,32 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.02,
+            length: 1
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
+    }
+    fly(){
+        this.sling.bodyA =null;
+    }
+    attach(body){
+        this.sling.bodyA=body;
+    }
+
+     display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+        
+            strokeWeight(4);
+            stroke("cyan");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
